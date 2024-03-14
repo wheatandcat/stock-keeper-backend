382aebed97676c47d8b0ab727d0e5b6f6a31d1f3
wheatandcat <wheatandcat@gmail.com>
2024-03-14 23:17:12 +0900

    fix

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,9 +1,20 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect()
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    const exitHandler = () => {
+      app.close()
+    }
+    process.on('exit', exitHandler)
+    process.on('beforeExit', exitHandler)
+    process.on('SIGINT', exitHandler)
+    process.on('SIGTERM', exitHandler)
+    process.on('SIGUSR2', exitHandler)
   }
 }
